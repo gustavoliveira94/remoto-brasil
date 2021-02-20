4120e0942bf9ce73ec2049dc205929321d136b68
Gustavo Oliveira <gmacedo@mcontigo.com>
2021-02-20 01:11:07 -0300

    refactor: eslint

--- .storybook/main.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  "stories": [
-    "../src/components/**/stories/stories.mdx",
-    "../src/components/**/stories/index.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-essentials"
-  ]
-}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
-const Home = () => {
-  return 'Full Remote BR';
-};
+import React from 'react';
+
+const Home: React.FC = () => <h1>Full Remote BR</h1>;
 
 export default Home;
