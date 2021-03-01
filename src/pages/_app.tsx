c7516b96c1e2eb55dad10b1307445848470fba81
Gustavo Oliveira <gmacedo@mcontigo.com>
2021-03-01 19:38:04 -0300

    feat: add tags twitter

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -17,11 +17,16 @@ const App = ({ Component, pageProps }: AppProps) => (
         content="Remoto Brasil - Uma plataforma de vagas remotas de Front End e Back End"
       />
       <meta name="keywords" content="remoto, home-office, home office, front end, back end, front-end, back-end, vagas remotas" />
+
       <meta property="og:title" content="Remoto Brasil - Vagas totalmente remotas!" />
       <meta property="og:description" content="Remoto Brasil - Uma plataforma de vagas remotas de Front End e Back End" />
       <meta property="og:type" content="website" />
       <meta property="og:url" content="https://remoto-brasil.com.br" />
       <meta property="og:site_name" content="Remoto Brasil" />
+
+      <meta property="twitter:url" content="https://remoto-brasil.com.br" />
+      <meta property="twitter:title" content="Remoto Brasil - Vagas totalmente remotas!" />
+      <meta property="twitter:description" content="Remoto Brasil - Uma plataforma de vagas remotas de Front End e Back End" />
     </Head>
     <ReposProvider>
       <GlobalStyles />
